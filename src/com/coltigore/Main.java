0c717d1bec33abf23a4ca71d3d7a484dd8d2773e
coltigore <alexsterling02@gmail.com>
2021-01-15 14:59:42 -0500

    Initial Commit

---
+++ src/com/coltigore/Main.java
@@ -0,0 +1,13 @@
+package com.coltigore;
+
+import java.awt.*;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        System.out.println(numbers);
+    }
+}
