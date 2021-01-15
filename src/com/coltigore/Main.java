198d89c71a771ac4f00e28c7b80f09eec08e2f13
coltigore <alexsterling02@gmail.com>
2021-01-15 15:20:11 -0500

    Added the sound file for notification, and starting to program sound playback, though I'm having issue with a { being expceted on line 23, but it's already a closed set of brackets.

--- src/com/coltigore/Main.java
+++ src/com/coltigore/Main.java
@@ -1,13 +1,23 @@
 package com.coltigore;
 
-import java.awt.*;
-import java.util.Locale;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        System.out.println(numbers);
+        File Ding = new File("Ding.WAV");
+        PlaySound(Ding);
+    }
+    static void PlaySound(File Sound) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(Sound));
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+        }catch (Exception e)
     }
 }
